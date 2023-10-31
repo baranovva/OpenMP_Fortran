c1780ead20670f9069cd99e711a0e489a37f9a66
baranovva <144531586+baranovva@users.noreply.github.com>
2023-10-31 23:09:02 +0300

    Create SECTIONS.f90

---
+++ Lab 1/SECTIONS.f90
@@ -0,0 +1,65 @@
+      PROGRAM OPENMP_3D_ARRAY
+      INCLUDE 'omp_lib.h'
+      INTEGER I, J, K, L, NI, NJ, NK, INFILE, OUTFILE
+      DOUBLE PRECISION T1, T2,S
+      DOUBLE PRECISION, ALLOCATABLE :: X(:,:,:), Y(:,:,:), Z(:,:,:)
+
+      OPEN(1, FILE="CUBE.MSH")
+      READ(1,*) NI, NJ, NK 
+      ALLOCATE (X(NI,NJ,NK), Y(NI,NJ,NK), Z(NI,NJ,NK))
+      DO K=1, NK 
+        DO J=1, NJ 
+         DO I=1, NI 
+          READ(1,*) X(I,J,K), Y(I,J,K), Z(I,J,K) 
+         END DO 
+        END DO
+      END DO
+      CLOSE(INFILE)
+
+      T1 = OMP_GET_WTIME()
+!$OMP PARALLEL PRIVATE(I,J,K)
+      DO L=1,10
+!$OMP SECTIONS
+!$OMP SECTION
+      DO K=2, NK-1
+       DO J=2, NJ-1
+        DO I=2, NI-1
+            S=abs((X(I+1,J,K) - X(I,J,K))*(X(I,J,K+1) - X(I,J,K)))
+        END DO
+       END DO
+      END DO
+!$OMP SECTION
+      DO K=2, NK-1
+       DO J=2, NJ-1
+        DO I=2, NI-1
+            S=abs((X(I,J+1,K) - X(I,J,K))*(X(I,J,K+1) - X(I,J,K)))
+        END DO
+       END DO
+      END DO
+!$OMP SECTION
+      DO K=2, NK-1
+       DO J=2, NJ-1
+        DO I=2, NI-1
+            S=abs((X(I+1,J,K) - X(I,J,K))*(X(I,J+1,K) - X(I,J,K)))
+        END DO
+       END DO
+      END DO
+!$OMP END SECTIONS
+      END DO
+!$OMP END PARALLEL
+      T2 = OMP_GET_WTIME()
+      PRINT *, 'computational time: ', T2-T1, 's'
+
+      OPEN(1, FILE="CUBE_NEW.MSH")
+      WRITE(1,*) NI, NJ, NK 
+      DO K=1, NK 
+        DO J=1, NJ 
+         DO I=1, NI 
+          WRITE(1,*) S 
+         END DO 
+        END DO
+      END DO
+      CLOSE(1)
+      DEALLOCATE(X,Y,Z)
+
+      END
